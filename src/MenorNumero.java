1661aae0d0166f562220417343e0807ffdd5eae4
ErivanB <Erivan.Pereira@academico.ifpb.edu.br>
2024-03-05 20:13:19 -0300

    teto numero impar

---
+++ src/MenorNumero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MenorNumero {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int[] lista ={14,12,78,45,3,67,9876,4};
+
+        int menor =  lista[0];
+
+        for(int numero : lista){
+
+            if(menor > numero){
+                menor = numero;
+            }
+
+        }
+        System.out.println("O MENOR NPUMERO É: "+menor);
+
+    }
+}
---
+++ src/SeuqenciaDeImpar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SeuqenciaDeImpar {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int  numTeto = teclado.nextInt();
+
+
+        for(int i = 1; i <= numTeto; i+=2){
+
+            System.out.println("impar:"+ i);
+        }
+    }
+}
